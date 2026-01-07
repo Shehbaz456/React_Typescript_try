7ee81863868142fcd36169442f20ba71e5704e3d
shehbaz <mdshehbaz666666@gmail.com>
2026-01-07 12:30:29 +0530

    Refactor ComponentButton to include variant styles and props

--- 01_basic/src/components/05_ComponentProps.tsx
+++ 01_basic/src/components/05_ComponentProps.tsx
@@ -1,13 +1,27 @@
-import type { ComponentProps } from "react"
+import type { ComponentProps, CSSProperties } from "react"
 
-type ButtonBaseProp 
+type ButtonBaseProp = ComponentProps<"button">
+type ButtonProp = ButtonBaseProp & {variant?: "primary"|"secondary" };
 
-function ComponentButton() {
-  return (
-    <div>
-      
-    </div>
-  )
+const variantStyles: Record<"primary" | "secondary", CSSProperties> = {
+  primary: {
+    backgroundColor: "blue",
+    color: "white",
+  },
+  secondary: {
+    backgroundColor: "gray",
+    color: "black",
+  },
+};
+
+function ComponentButton({variant="primary",style,...rest}:ButtonProp) {
+    const base : CSSProperties ={
+        padding:"12px",
+        borderRadius: "6px",
+        border: "none",
+        cursor: "pointer",
+    }
+  return ( <button style={{...base,...variantStyles[variant],...style}} {...rest} >Click</button> )
 }
 
 export default ComponentButton
